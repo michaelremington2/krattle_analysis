c21c256f7382e3ab1c0e8ccdad35f5f8cab3538e
Michael Remington <michaelremington2@gmail.com>
2021-07-25 21:51:17 -0700

    Developed a script to aggregate data from many simulations then made an r script to analyze the output.

--- agg_sims.py
+++ agg_sims.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
 import csv
 import sys
 import argparse
 import pandas as pd 
 import glob
 import re
+import json
 
 
 #print(sys.argv[0])
@@ -13,37 +15,73 @@ import re
 
 
 #for i in
-def create_csv(output_file_path):
-	with open(output_file_path, "w") as my_empty_csv:
-		pass
-
-def extract_info(sims):
-	#,output_file_path
-	for sim in sims:
-		sim_number = re.findall(r'\d+',sim)[-1]
-		print(sim.split("/")[-1].split("_")[0])
+class export_data_from_sims(object):
+	def __init__(self,sims, output_file_path_total, output_file_path_per_cycle=None):
+		self.sims = sims
+		self.output_file_path_total = output_file_path_total
+		self.output_file_path_per_cycle = output_file_path_per_cycle
 
 
+	def create_csv(self,fp):
+		with open(fp, "w") as my_empty_csv:
+			pass
+
+	def format_experiment_label(self,exp_label):
+		if len(exp_label.split("_")[1]) <= 2:
+			experiment = exp_label.split("_")[0]+exp_label.split("_")[1]
+		else:
+			experiment = exp_label.split("_")[0]
+		return experiment
+		
+
+	def overall_stats(self, sim):
+		data=pd.read_csv(sim,header=None)
+		data.columns = ['id','generation', 'cycle','open_pw','bush_pw','energy_score','movements','cell_id','microhabitat','snakes_in_cell','owls_in_cell']
+		cycles=data['cycle'].max()
+		generations=data['generation'].max()
+		mean_bush_pref=data['bush_pw'].mean()
+		std_bush_pref=data['bush_pw'].std()
+		se_bush_pref=data['bush_pw'].sem()
+		return cycles, generations, mean_bush_pref, std_bush_pref, se_bush_pref
 
 
-def append_data(d_row,output_file_path):
-	with open(output_file_path, 'a') as f:
-	    writer = csv.writer(f)
-	    writer.writerow(d_row)
+	def extract_info(self):
+		#,output_file_path
+		self.create_csv(fp = self.output_file_path_total)
+		for sim in self.sims:
+			exp_label = sim.split("/")[-1]
+			experiment = self.format_experiment_label(exp_label = exp_label)
+			sim_number = re.findall(r'\d+',sim)[-1]
+			cycles, generations, mean_bush_pref, std_bush_pref, se_bush_pref = self.overall_stats(sim =sim)
+			if 'krat' in exp_label:
+				data_type='krat'
+			if 'snake' in exp_label:
+				data_type='snake'
+			row = [exp_label, experiment, sim_number, data_type, cycles, generations, mean_bush_pref, std_bush_pref, se_bush_pref]
+			self.append_data(fp = self.output_file_path_total,d_row = row)
+
+	def append_data(self,fp,d_row):
+		with open(fp, 'a') as f:
+		    writer = csv.writer(f)
+		    writer.writerow(d_row)
 
 
 def run(args):
     directory = args.input
+    output_file_path_total = args.output_file_path_total
     sims = glob.glob(directory + "*.csv")
-    extract_info(sims = sims)
+    edfs = export_data_from_sims(sims = sims, output_file_path_total=output_file_path_total)
+    edfs.extract_info()
 
     #output_file_path = args.output # from dest="output"
 
 
 def main():
+	# python agg_sims.py -in  owl_data/sim_data/ -totals owl_data/total_sims_stats.csv
     parser=argparse.ArgumentParser(description="Aggregate the results and output a csv of them.")
     parser.add_argument("-in",help="a directory full of csvs." ,dest="input", type=str, required=True)
-    #parser.add_argument("-out",help="a file path for the output csv" ,dest="output", type=str, required=False, default=None)
+    parser.add_argument("-totals",help="a file path for the output csv of the stat on all simulations" ,dest="output_file_path_total", type=str, required=False, default=None)
+    #parser.add_argument("-exp_config_file",help="the file of the config for the experiment" ,dest="config", type=str, required=False, default=None)
     parser.set_defaults(func=run)
     args=parser.parse_args()
     args.func(args)
--- rdataanalysis.R
+++ rdataanalysis.R
@@ -1,4 +1,26 @@
 library(dplyr)
+library(ggplot2)
+ls()
+setwd("/home/mremington/Documents/krattle_analysis/krattle_analysis/owl_data/")
+total_data <- read.csv("total_sims_stats.csv",header=FALSE)
+total_data <- total_data %>% 
+  rename(
+    file = V1,
+    experiment = V2,
+    sim = V3,
+    org = V4,
+    end_cycle = V5,
+    end_gen = V6,
+    avg_bush_pref = V7,
+    std_bush_pref = V8,
+    se_bush_pref = V9
+  )
+## Krat
+krat_info <- total_data %>% filter(org=="krat")
+krat_bp <- krat_info %>% group_by(experiment) %>% summarise(avg_bp = mean(avg_bush_pref))
+ggplot(krat_bp,aes(x=experiment,y=avg_bp))+geom_bar(stat='identity')+ggtitle('Krat AVG bush Preference')
 
-temp = list.files(pattern="/home/mremington/Documents/krattle_analysis/krattle_analysis/Data/*.csv")
-myfiles = lapply(temp, read.delim)
+## Snake
+snake_info <- total_data %>% filter(org=="snake")
+snake_bp <- snake_info %>% group_by(experiment) %>% summarise(avg_bp = mean(avg_bush_pref))
+ggplot(snake_bp,aes(x=experiment,y=avg_bp))+geom_bar(stat='identity')+ggtitle('snake AVG bush Preference')
