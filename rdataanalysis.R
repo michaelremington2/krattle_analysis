3845cc1151cffaf3ac3554cc6508b09a3c7d39ea
Michael Remington <michaelremington2@gmail.com>
2021-07-28 23:47:34 -0700

    finished script that aggregated info of many simulations and added figures in r script to visualize aggregations by cycle.

--- rdataanalysis.R
+++ rdataanalysis.R
@@ -24,3 +24,31 @@ ggplot(krat_bp,aes(x=experiment,y=avg_bp))+geom_bar(stat='identity')+ggtitle('Kr
 snake_info <- total_data %>% filter(org=="snake")
 snake_bp <- snake_info %>% group_by(experiment) %>% summarise(avg_bp = mean(avg_bush_pref))
 ggplot(snake_bp,aes(x=experiment,y=avg_bp))+geom_bar(stat='identity')+ggtitle('Snake AVG bush Preference')
+
+
+#########################
+##### Per cycle Analysis
+#########################
+
+per_cycle <-read.csv("owl_sim_stats_per_cycle.csv", header=FALSE)
+per_cycle <- per_cycle %>% 
+  rename(
+    file = V1,
+    experiment = V2,
+    sim = V3,
+    org = V4,
+    cycle = V5,
+    avg_bush_pref= V6
+  )
+
+## Krat
+krat_info_per_cycle <- per_cycle %>% filter(org=="krat", experiment== "control")
+krat_bp_per_cycle <- krat_info_per_cycle %>% group_by(experiment,cycle) %>% summarise(avg_bp = mean(avg_bush_pref))
+ggplot(krat_bp_per_cycle, aes(x=cycle, y=avg_bp, group=experiment, color=experiment)) +
+  geom_line()+ggtitle('Krat AVG bush Preference')
+
+## Snake
+snake_info_per_cycle <- per_cycle %>% filter(org=="snake")
+snake_bp_per_cycle <- snake_info_per_cycle %>% group_by(experiment,cycle) %>% summarise(avg_bp = mean(avg_bush_pref))
+ggplot(snake_bp_per_cycle, aes(x=cycle, y=avg_bp, group=experiment, color=experiment))+
+  geom_line()+ggtitle('Snake AVG bush Preference')
